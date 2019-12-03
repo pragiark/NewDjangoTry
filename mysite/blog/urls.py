f133e27e2e45348dc04c156e3efe2d73c5969e76
pragiark <pragiark@wp.pl>
2019-12-03 23:18:32 +0100

    Change def to class in post views

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = 'blog'
-
 urlpatterns = [
     # widoki posta
-    path('', views.post_list, name='post_list'),
+    #path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
