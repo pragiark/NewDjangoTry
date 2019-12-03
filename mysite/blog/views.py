ac6fbe5740f08132ee4af3d1651aa83f8f03409e
pragiark <pragiark@wp.pl>
2019-12-03 23:17:54 +0100

    Use Class in views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 2) # three post on page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Jeśli zmienna page niejest liczbą całkowitą
+        # Wówczas pobeirana jest pierwsza strona z wyników
+        posts = paginator.page(1)
+    except EmptyPage:
+        #Jeśli zmienna page ma wartość wiekszą niż numer ostatniej strony
+        #wyników, wtedy pobierana jest ostatnia strona wyników
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
-    post = get_list_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
                            publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
