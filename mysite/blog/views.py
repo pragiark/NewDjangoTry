b8e60a048c21d1b9c2e9133c1b61345b776f08ee
pragiark <pragiark@wp.pl>
2020-04-18 22:04:34 +0200

    Fix bugs in views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -22,10 +22,7 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
-                           publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+
 
 class PostListView(ListView):
     queryset = Post.published.all()
